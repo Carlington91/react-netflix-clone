0a18333f0c56b320f3e3861249e51c07958c1eee
carlington91 <floridaelite@ymail.com>
2021-01-07 21:25:52 -0500

    update

--- app.js
+++ app.js
@@ -11,17 +11,17 @@ if (process.env.NODE_ENV !== 'production') {
   app.use(require('cors')());
 }
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 //autoload routes
 fs.readdirSync('./routes').map((router) => {
   const route = router.split('.')[0];
   app.use('/api/v1/' + route, require('./routes/' + router));
 });
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 module.exports = app;
